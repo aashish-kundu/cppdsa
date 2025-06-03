b87e783fb8bb5cac475f0478f6b518a270809234
AASHISHKUNDU <KUNDUJAAT103@GMAIL.COM>
2025-06-04 00:20:07 +0530

    first commit

---
+++ cpp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age;
+    cout << "Your Age : " << endl;
+    cin >> age;
+
+    if (age >= 18)
+    {
+        cout << "Ashish you are an Adult" << endl;
+    }
+    else
+    {
+        cout << "Ashish you are not an Adult" << endl;
+    }
+
+    return 0;
+}
