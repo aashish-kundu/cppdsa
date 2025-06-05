7c053c4e71fcc232f01836a55b5d8ebbe17a5f46
Aashish kundu <ashishstudy103@gmail.com>
2025-06-05 14:28:45 +0530

    commit

---
+++ Learnarray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    
+
+  int scores[] = {90, 85, 95, 100};
+   cout << scores[0] << endl;
+
+   char name[] = "Aashishkundu";
+   cout << name << endl;
+
+ int matrix[7] [10];
+
+  matrix[0] [1] =10,11;
+  cout<<matrix[0] [1];
+
+
+    return 0;
+}
